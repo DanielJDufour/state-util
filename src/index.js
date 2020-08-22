67dde3638f29b0882c77d33e7545379b3f5bba8b
DanielJDufour <daniel.j.dufour@gmail.com>
2020-08-22 13:38:30 -0500

    added connectStateToElement to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    connectStateToElement: require('./connect-state-to-element.js'),
     writeStateToElement: require('./write-state-to-element.js'),
     writeStateEntryToElement: require('./write-state-entry-to-element.js')
 };
